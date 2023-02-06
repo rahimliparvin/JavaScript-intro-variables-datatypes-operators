03ac11ad5d73b80d2141def433ec6331eeeb5ac0
rahimliparvin <parvinvr@code.edu.az>
2023-02-06 11:51:19 -0800

    new add file

---
+++ assets/js/script.js
@@ -0,0 +1,192 @@
+// "use strict"
+
+// console.log("Hello");
+
+// alert("Salam Cavid");
+
+// var ,let ,const
+
+//var name = "Cavid";
+
+//let surname = "Ismayilzade";
+
+// const ISMARRIED = true;
+
+// var surname = "Ismayilzade";
+
+// var name =  "Cavid";
+
+// console.log(surname+ " " +name);
+
+// var name = "Aydin";
+
+// console.log(name);
+
+
+
+// if(true){
+//     var age = 55;
+// }
+
+// console.log(age);
+
+//let age = 36;
+
+//if(true){
+    // let age = 25;
+   // console.log(age);
+//}
+
+// console.log(age);
+
+
+// const age = 55;
+
+// age = 70;
+
+// console.log(age);
+
+
+// let age = 25;
+
+// let  name = "Aqshin";
+
+// let ISMARRIED = true;
+
+// let phone;
+
+// let  address = null;
+
+// console.log(typeof age);
+
+// console.log(typeof name);
+
+// console.log(typeof ISMARRIED);
+
+// console.log(typeof phone);
+
+// console.log(typeof address);
+
+
+
+// if(phone == undefined){
+
+//     console.log("phone underfined oldu");
+// }
+
+
+// let stu = {
+
+//     name : "Cavid",
+//     surname : "Ismayilzade",
+// }
+
+// console.log(stu.name + " " + stu.surname);
+
+// console.log(`${stu.name} ${stu.surname}`);
+
+// let students = ["Cavid" , "Pervin" , "Mirze" , 100, 200 ,true];
+
+// console.log("Cavid"/3);
+
+
+
+// for(let i = 0 ; i < students.length ; i++ ){
+
+//     console.log(students[i]);
+// }
+
+
+// alert("Cavid");
+
+// let name = prompt("Please add your name");
+
+// debugger
+
+// console.log(name);
+
+// let ISONLINE = confirm("is he online ?");
+
+// console.log(ISONLINE);
+
+// if(ISONLINE){
+//     console.log("he is online")
+// }else{
+//     console.log("he is offline")
+// }
+
+// let age = 20;
+
+// if(age > 15){
+//     console.log("boyukdur");
+// }
+// else{
+//     console.log("kicikdir");
+// }
+
+// let temp = 15;
+// if(temp > 10){
+//     console.log("istidir")
+// }else if(temp<10){
+//     console.log("soyuqdur")
+// }else{
+//     console.log("hec biri")
+// }
+
+// let count = 66;
+
+//    switch (count) {
+//     case 5:
+//         console.log( "besh")
+//         break;
+//         case 6:
+//             console.log( "alti")
+//             break;
+//     default:
+//         console.log("not  found")
+//         break;
+//    }
+
+
+// for(let i = 0; i < 20 ; i++){
+//     console.log(i);
+// }
+
+// let result = 2**3;
+// console.log(result);
+
+// console.log("44"/2);
+
+// console.log("44"+2);
+
+// console.log("44"-2);
+
+// console.log("44"*2);
+
+// console.log("44c"/2);
+
+// let ISMARRIED = false;
+
+// let result = ISMARRIED?"Evlidir":"Subaydir";
+
+// console.log(result);
+
+
+// let address = null;
+
+// let result1 = address??"Ehmedli";
+
+// console.log(result1);
+
+// let addresss = "Xizi";
+
+// let result2 = addresss??"Ehmedli";
+
+// console.log(result2);
+
+
+let  address = "Xizi";
+
+var result = address == null? "Ehmedli": address;
+
+console.log(result);
